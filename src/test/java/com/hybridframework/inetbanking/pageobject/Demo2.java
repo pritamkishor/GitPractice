0fcbb8eb13678b578e2a05ea279d560b71fe3986
PRITAM <charmprit69@gmail.com>
2022-05-17 21:29:01 +0530

    Commited from GitPractice Repo Guy

---
+++ src/test/java/com/hybridframework/inetbanking/pageobject/Demo2.java
@@ -0,0 +1,11 @@
+package com.hybridframework.inetbanking.pageobject;
+
+public class Demo2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello All");
+		System.out.println("Hello All");
+	}
+
+}
